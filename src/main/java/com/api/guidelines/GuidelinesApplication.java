773189d0b90284d41937ce84916c9132a69d9bc7
leonelmfonseca <leonelfonseca@gmail.com>
2025-02-13 16:59:01 +0000

    feat: init commit

---
+++ src/main/java/com/api/guidelines/GuidelinesApplication.java
@@ -0,0 +1,13 @@
+package com.api.guidelines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuidelinesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuidelinesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/guidelines/GuidelinesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.guidelines;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuidelinesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
