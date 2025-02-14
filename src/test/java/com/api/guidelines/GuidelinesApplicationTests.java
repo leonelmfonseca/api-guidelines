9c22faaef4b9787a6811b200868149e28b369979
leonelmfonseca <leonelfonseca@gmail.com>
2025-02-14 17:27:20 +0000

    test: add comment to shut up SonarIDE

--- src/test/java/com/api/guidelines/GuidelinesApplicationTests.java
+++ src/test/java/com/api/guidelines/GuidelinesApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 class GuidelinesApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads() { // default implementation ignored
 	}
 
 }
