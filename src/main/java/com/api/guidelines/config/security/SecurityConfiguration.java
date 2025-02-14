f3a94f83bdb6e64b7afba99bad7d73dd3f91f1f9
leonelmfonseca <leonelfonseca@gmail.com>
2025-02-14 17:25:58 +0000

    feat(security): authentication and authorization

--- src/main/java/com/api/guidelines/config/security/SecurityConfiguration.java
+++ src/main/java/com/api/guidelines/config/security/SecurityConfiguration.java
@@ -1,4 +1,4 @@
-package com.api.guidelines.config;
+package com.api.guidelines.config.security;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -6,17 +6,23 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
 
-  private static final String[] PUBLIC_ENDPOINTS = {"/api/v1/products", "/api/v2/products"};
+  // To match all subpaths under the specified endpoints use "/**"
+  private static final String[] PUBLIC_ENDPOINTS = {
+    "/api/v1/products/**", "/api/v2/products/**", "/api/products/**"
+  };
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    http.authorizeHttpRequests(
+    http // Disable CSRF (Cross-Site Request Forgery) for stateless APIs
+        .csrf(AbstractHttpConfigurer::disable)
+        .authorizeHttpRequests(
             auth -> auth.requestMatchers(PUBLIC_ENDPOINTS).permitAll().anyRequest().authenticated())
         .httpBasic(withDefaults());
 
