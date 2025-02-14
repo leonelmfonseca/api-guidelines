51f95dbc3be3ac466ec8f0d035fe65df68646b7a
leonelmfonseca <leonelfonseca@gmail.com>
2025-02-14 17:26:35 +0000

    refactor: code structure

--- src/main/java/com/api/guidelines/service/ProductService.java
+++ src/main/java/com/api/guidelines/service/ProductService.java
@@ -2,6 +2,7 @@ package com.api.guidelines.service;
 
 import com.api.guidelines.model.Product;
 import java.util.List;
+import java.util.Optional;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.stereotype.Service;
@@ -10,18 +11,22 @@ import org.springframework.stereotype.Service;
 @Service
 @AllArgsConstructor
 public class ProductService {
-    
-    private List<Product> products;
-    
-    public List<Product> findAllV1(){
-        return products;
-    }
-    
-    public List<Product> findAllV2(){
-        return products.stream().filter(p->p.getStock()>0).toList();
-    }
-    
-    public void addProduct(Product product) {
-        products.add(product);
-    }
+
+  private List<Product> products;
+
+  public List<Product> findAllV1() {
+    return products;
+  }
+
+  public List<Product> findAllV2() {
+    return products.stream().filter(p -> p.getStock() > 0).toList();
+  }
+
+  public void addProduct(Product product) {
+    products.add(product);
+  }
+
+  public Optional<Product> findProductById(Long id) {
+    return products.stream().filter(p -> p.getId().equals(id)).findFirst();
+  }
 }
