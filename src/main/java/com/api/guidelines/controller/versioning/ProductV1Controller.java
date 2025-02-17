d2ddc87a8cc7ea903f1d9c75346129b5ea56d5e4
leonelmfonseca <leonelfonseca@gmail.com>
2025-02-17 19:26:36 +0000

    feat(api): api versioning demo

--- src/main/java/com/api/guidelines/controller/versioning/ProductV1Controller.java
+++ src/main/java/com/api/guidelines/controller/versioning/ProductV1Controller.java
@@ -1,6 +1,6 @@
 package com.api.guidelines.controller.versioning;
 
-import com.api.guidelines.model.Product;
+import com.api.guidelines.dto.ProductDTO;
 import com.api.guidelines.service.ProductService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,12 +8,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-  /*
-  API versioning in Java is a crucial best practice for maintaining backward compatibility
-  and ensuring a smooth transition.
-  API versioning can be implementing using URL segments, query parameters, or headers to ensure
-  backward compatibility and smooth transitions between versions
-  */
+/*
+API versioning in Java is a crucial best practice for maintaining backward compatibility
+and ensuring a smooth transition.
+API versioning can be implementing using URL segments, query parameters, or headers to ensure
+backward compatibility and smooth transitions between versions
+*/
 
 // API versioning in Java is a crucial best practice for
 // maintaining backward compatibility and ensuring a smooth transition
@@ -21,8 +21,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/products")
 public class ProductV1Controller {
-
-  ProductService productService;
+  
+  private final ProductService productService;
 
   @Autowired
   public ProductV1Controller(ProductService productService) {
@@ -30,7 +30,7 @@ public class ProductV1Controller {
   }
 
   @GetMapping
-  public List<Product> findAllV1() {
-    return productService.findAllV1();
+  public List<ProductDTO> findAllV1() {
+    return productService.getAllProductsV1();
   }
 }
--- src/main/java/com/api/guidelines/controller/versioning/ProductV2Controller.java
+++ src/main/java/com/api/guidelines/controller/versioning/ProductV2Controller.java
@@ -1,15 +1,15 @@
 package com.api.guidelines.controller.versioning;
 
-import com.api.guidelines.model.Product;
+import com.api.guidelines.dto.ProductDTO;
 import com.api.guidelines.service.ProductService;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 // API versioning in Java is a crucial best practice for
@@ -18,13 +18,13 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestController
 @RequestMapping("/api/v2/products")
 /*
- The RestControllerAdvice annotation is suitable for Spring Boot applications serving RESTful services
- where the controllers return data in the form of JSON or other serialization formats
- */
+The RestControllerAdvice annotation is suitable for Spring Boot applications serving RESTful services
+where the controllers return data in the form of JSON or other serialization formats
+*/
 @RestControllerAdvice
 public class ProductV2Controller {
-
-  ProductService productService;
+  
+  private final ProductService productService;
 
   @Autowired
   public ProductV2Controller(ProductService productService) {
@@ -32,16 +32,17 @@ public class ProductV2Controller {
   }
 
   @GetMapping
-  public List<Product> findAllV2() {
-    return productService.findAllV2();
+  public List<ProductDTO> findAllV2() {
+    return productService.getAllProductsV2();
   }
-  
+
   @GetMapping("/{id}")
-  public ResponseEntity<Product> findProductById(@PathVariable Long id) {
-   
-    Optional<Product> product = productService.findProductById(id);
-    
-    return product.map(ResponseEntity :: ok) // Return 200 with product
-            .orElse(ResponseEntity.notFound().build()); // Return 404 if not found
+  public ResponseEntity<ProductDTO> findProductById(@PathVariable Long id) {
+
+    Optional<ProductDTO> product = productService.getProductById(id);
+
+    return product
+        .map(ResponseEntity::ok) // Return 200 with product
+        .orElse(ResponseEntity.notFound().build()); // Return 404 if not found
   }
 }
